80adbe51883385d18101a75a15f94fd34a1e2e06
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-22 00:26:39 +0300

    PostgreSQ

--- examples/index.php
+++ examples/index.php
@@ -4,17 +4,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Pavlyshyn\Orm;
 use Pavlyshyn\DB\Driver\PostgreSQL;
+use Pavlyshyn\DB\Driver\MySQL;
+use Pavlyshyn\DB\Driver\SQLite;
 
 use \Model\Users;
 
 
 
-$orm = new Orm(new PostgreSQL('localhost', 'test_orm', 'postgres', 'password'));
+//$orm = new Orm(new PostgreSQL('localhost', 'test_orm', 'postgres', 'password'));
+
+//$orm = new Orm(new MySQL('localhost', 'test_orm', 'root', 'password'));
+
+$orm = new Orm(new SQLite('test_orm.sqlite'));
+
 
 // INSERT
 ///*
 $user = new Users();
-$user->id = 3;
+//$user->id = 3;
 $user->name = 'Username4';
 $user->mail = 'username@mail.com';
 $user->password = sha1('password');
