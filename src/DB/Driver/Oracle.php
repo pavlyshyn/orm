c30f074e39d9b89b43b0f4869fc1cbab33dafecf
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-26 01:52:27 +0300

    Oracle driver

---
+++ src/DB/Driver/Oracle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pavlyshyn\DB\Driver;
+
+class Oracle extends \Pavlyshyn\DB\Driver\SQL {
+
+    public function __construct($host, $db, $user, $password, $character = 'utf8') {
+        try {
+            $this->connection = new \PDO('oci:host=' . $host . ';dbname=' . $db, $user, $password);
+
+            $this->connection->query('SET NAMES ' . $character . ';');
+        } catch (Pavlyshyn\Exception $e) {
+            echo 'Error Oracle connection (' . $e->getCode() . '): ', $e->getMessage(), "\n";
+        }
+    }
+
+}
