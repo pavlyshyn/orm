26e21f38c9a0826ee98b2e8694bfcd8894c0dc7d
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-26 23:14:49 +0300

    result iterator

---
+++ src/DB/PDOResultIterator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pavlyshyn\DB;
+
+use \PDO;
+
+class PDOResultIterator implements Pavlyshyn\Data\Iterator {
+
+    private $stmt = null;
+    private $result = null;
+
+    public function __construct($pdo, $q) {
+        $this->stmt = $pdo->prepare($q);
+        $this->stmt->execute();
+    }
+
+    public function current() {
+        return $this->result;
+    }
+
+    public function key() {
+        if (!empty($this->result->id)) {
+            return $this->result->id;
+        }
+    }
+
+    public function next() {
+        $this->result = $this->stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_NEXT);
+    }
+
+    public function rewind() {
+        $this->result = $this->stmt->fetch(PDO::FETCH_OBJ, PDO::FETCH_ORI_PRIOR);
+    }
+
+    public function valid() {
+        return ($this->result != null);
+    }
+
+}
---
+++ src/Data/Iterator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pavlyshyn\Data;
+
+/**
+ * Interface for external iterators or objects that can be iterated
+ * themselves internally.
+ * @link http://php.net/manual/en/class.iterator.php
+ */
+interface Iterator extends Traversable {
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    abstract public function current();
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    abstract public function next();
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return scalar scalar on success, or <b>NULL</b> on failure.
+     */
+    abstract public function key();
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns <b>TRUE</b> on success or <b>FALSE</b> on failure.
+     */
+    abstract public function valid();
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    abstract public function rewind();
+}
