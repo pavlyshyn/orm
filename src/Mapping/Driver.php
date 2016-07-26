53755a3ef34728605da20f0db7a5f9c2e375e8c1
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-07-26 05:48:44 +0300

    init mapping

---
+++ src/Mapping/Driver.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pavlyshyn\Mapping;
+
+interface Driver {
+    
+}
---
+++ src/Mapping/Driver/PHP.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pavlyshyn\Data\Type;
+
+use Pavlyshyn\Mapping\Driver;
+
+class PHP extends Driver {
+    
+}
